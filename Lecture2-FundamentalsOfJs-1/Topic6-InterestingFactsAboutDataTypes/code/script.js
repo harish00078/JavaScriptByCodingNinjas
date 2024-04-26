e32c862f0939f71d95f1be590b225ded364d2537
harish kumar <harishpathania150@gmail.com>
2024-04-26 19:23:10 +0530

    Interesting facts about number Primitive-datatypes

--- Lecture2-FundamentalsOfJs-1/Topic6-InterestingFactsAboutDataTypes/code/script.js
+++ Lecture2-FundamentalsOfJs-1/Topic6-InterestingFactsAboutDataTypes/code/script.js
@@ -1,2 +1,18 @@
-alert('Interesting Facts About Data Types');
+console.log('Interesting Facts About Primitive-Data-Types');
+// Here we have only see the Interesting facts about (Number) Primitive-Data-Types:
 
+// => 1 = first we have:here is (MAX_VALUE):
+console.log(Number.MAX_VALUE);
+// => 2 = second we have:here is (MIN_VALUE):
+console.log(Number.MIN_VALUE);
+// IMP-NOTE = these both are not the maximum and minimum values we can have in the javascript:
+// In js we also have (+Infinity) and (-Infinity):
+// => 1 = first we have:here is (+Infinity):
+console.log(Number.MAX_VALUE *10);
+// => 2 = second we have:here is (-Infinity):
+console.log(Number.MIN_VALUE * 10);
+// IMP = here we are using the (Infinity) values:
+// if any thing divided with the (zero):then it will gave us the result in (infinite) or (+Infinite):
+console.log(12/0);
+// if any thing gets divided with the (Infinity) direclty:then it will gave us the result in (zero):
+console.log(188/Infinity);
