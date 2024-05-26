1120940350a7933a365693712e816b51dbe94d52
harish kumar <harishpathania150@gmail.com>
2024-05-26 12:18:53 +0530

    learning about operators in js

--- Lecture3-FundamentalsOfJs-2/Topic1-OperatorsInJs/code-withNewLectures/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic1-OperatorsInJs/code-withNewLectures/script.js
@@ -1,1 +1,19 @@
 console.log("Learn about Operator in js");
+// Operators in js:operators are special symbols that perform operations on values (operands). These operations can be anything from basic arithmetic calculations to complex comparisons and data manipulations. They're fundamental building blocks for writing JavaScript code as they control the flow and processing of data with in your program:
+// Operators are mainly of five types:
+// 1 = Arithematic Operators:(+,-,*,/,%,**,++,--)
+// 2 = Assignment Operators:(=,+=,-=,*=,/=,%=,**==)
+// 3 = Comparison Operators:(>,<,<=,>=,==,!=,===,!==)
+// 4 = Logical Operators:(&&,||,!)
+// 5 = Bitwise Operators:(&,|,^,~,<<,>>,>>> )
+
+// => here in this topic we gonna be learn about:these operators:
+// 1 = Assignment Operators:
+// 2 = Arithematic Operators:
+// 3 = more Arithematic Operators:
+// 4 = Augmented Assignment Operators:
+
+// 1 = Assignment Operators:these operators are used to assign a value to the variable:(=,+=,-=,*=,/=,%=,**==)
+// IMP = these operators baiscally work from (right) to (left):
+
+let a = 5;
