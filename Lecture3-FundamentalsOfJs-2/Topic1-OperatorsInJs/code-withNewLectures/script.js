42e6b37b8f8ecfd2c724ad2b33933badc0da86e2
harish kumar <harishpathania150@gmail.com>
2024-06-01 13:50:45 +0530

    learning about operators in js

--- Lecture3-FundamentalsOfJs-2/Topic1-OperatorsInJs/code-withNewLectures/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic1-OperatorsInJs/code-withNewLectures/script.js
@@ -2,7 +2,7 @@ console.log("Learn about Operator in js");
 // Operators in js:operators are special symbols that perform operations on values (operands). These operations can be anything from basic arithmetic calculations to complex comparisons and data manipulations. They're fundamental building blocks for writing JavaScript code as they control the flow and processing of data with in your program:
 // Operators are mainly of five types:
 // 1 = Arithematic Operators:(+,-,*,/,%,**,++,--)
-// 2 = Assignment Operators:(=,+=,-=,*=,/=,%=,**==)
+// 2 = Assignment Operators:(=,+=,-=,*=,/=,%=,**=)
 // 3 = Comparison Operators:(>,<,<=,>=,==,!=,===,!==)
 // 4 = Logical Operators:(&&,||,!)
 // 5 = Bitwise Operators:(&,|,^,~,<<,>>,>>> )
@@ -13,7 +13,52 @@ console.log("Learn about Operator in js");
 // 3 = more Arithematic Operators:
 // 4 = Augmented Assignment Operators:
 
-// 1 = Assignment Operators:these operators are used to assign a value to the variable:(=,+=,-=,*=,/=,%=,**==)
-// IMP = these operators baiscally work from (right) to (left):
+// => 1 = Assignment Operators:(=,+=,-=,*=,/=,%=,**==):these operators are used to assign a value to the variable:
+ 
+// IMP = these Assignment-operators baiscally work from (right) to (left) not from (left) to (right) other then like other operators:and it called (associativity):
+// In associativity.first the values get resolved.and then they get assigned to its variable:
 
-let a = 5;
+// -> here we use assign (equalto) or simple-assignment operator:
+let a = 10;
+// IMP = example of associativity:
+// -> In this equation or variable:we can see that because of its (right) to (left) associativity:first (a) get resolved and after that its value get assigned to the (b) variable:
+let b = a;
+// -> because of its Associativity:we can also assign the one value to multiple variables in one-equation or one-variable declaration:
+let c = a = 20;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+// IMP = these are all other assignment operators working:
+// -> addition assignment:
+let d = 5;
+d += 2;
+console.log(d);
+// -> substraction assignment:
+let e = 5;
+e -= 2;
+console.log(e);
+// -> multiplication assignment:
+let multi = 5;
+multi *= 2;
+console.log(multi);
+// -> division assignment:
+let f = 5;
+f /= 2;
+console.log(f);
+// -> modulus assignment:
+let g = 5;
+g %= 2;
+console.log(g);
+// -> exponent or power assignment:
+let h = 5;
+h **= 2;
+// here we are doing the (raise to the power):
+console.log(h);
+
+// => 2 = Arithematic Operators:(+,-,*,/,%,**,++,--):
+
+
+
