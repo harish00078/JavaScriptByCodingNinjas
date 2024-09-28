6a88f4b6263d66da5cfe865f10993cd99148427d
harish kumar <harishpathania150@gmail.com>
2024-09-28 21:26:58 +0530

    klearning about operatos

--- Lecture3-FundamentalsOfJs-2/Topic2-ComparisonOperators/Comparison-Operator.js
+++ Lecture3-FundamentalsOfJs-2/Topic2-ComparisonOperators/Comparison-Operator.js
@@ -1,4 +1,11 @@
 // Here we gonna be learn about (Comparison-Operator):
-console.log('Learning About Comparison Operator');
+console.log("Learning About Comparison Operator");
 
 // => 1 = Comparison-Operator:(>,<,<=,>=,==,!=,===,!==):
+// V.IMP = comparison-operators basically return us the boolean values:These operators compare two values and return a boolean value (true or false) to us  based on the comparison result:
+// V.IMP = the two of these comparison operators:you will be only find in the javascript language(===,!==):
+// -> (===) = strict-equality-operator:
+// -> (!==) = strict-inequality-operator:
+
+// => here gonna check them one by one:
+// => 1 = (==): Its known as (equality) operator:It compares the values of two operands for equality but without any type conversion. It returns true if the values are equal and have the same type or same (data-type):
