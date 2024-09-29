8f83fde27f95787fcf662a6aa7b13d5fb92d4b0e
harish kumar <harishpathania150@gmail.com>
2024-09-29 19:25:51 +0530

    learned some basic comparison operators

--- Lecture3-FundamentalsOfJs-2/Topic2-ComparisonOperators/Comparison-Operator.js
+++ Lecture3-FundamentalsOfJs-2/Topic2-ComparisonOperators/Comparison-Operator.js
@@ -5,8 +5,19 @@ console.log("Learning About Comparison Operator");
 // V.IMP = comparison-operators basically return us the boolean-values:These operators compare two values and return a boolean value (true or false) to us  based on there comparison-result:
 
 // V.IMP = the two of these comparison operators:you will be only find in the javascript language(===,!==):
-// -> (===) = strict-equality-operator:
-// -> (!==) = strict-inequality-operator:
+// -> (===) = strict-equality(comparison)-operator:
+// -> (!==) = strict-inequality(comparison)-operator:
 
 // => here gonna check them one by one:
 // => 1 = (==): Its known as (equality) operator:It compares the values of two operands for equality but without any type conversion. It returns true if the values are equal and have the same type or same (data-type):
+console.log(5==5);
+// => 2 = (!=):  Its known as (inequality) operator:It compares the values of two operands for inequality but without any type conversion. It returns true if the values are not equal and have the same type or same (data-type):
+console.log(5!=6);
+// => 3 = (<): Its known as (less-than) operator:It compares the values of two operands and returns true if the value of the left operand is less than the value of the right operand:
+console.log(5<6);
+// => 4 = (>): Its known as (greater-than) operator:It compares the values of two operands and returns true if the value of the left operand is greater than the value of the right operand:
+console.log(6>5);
+// => 5 = (<=): Its known as (less-than-equal-to) operator:It compares the values of two operands and returns true if the value of the left operand is less than or equal to the value of the right operand:
+console.log(5<=5);
+//  => 6 = (>=): Its known as (greater-than-equal-to) operator:It compares the values of two operands and returns true if the value of the left operand is greater than or equal to the value of the right operand:
+console.log(5>=5);
