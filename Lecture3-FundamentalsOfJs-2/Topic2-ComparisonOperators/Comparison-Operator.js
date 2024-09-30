3cb433218be99f027b47d4c734a95fd873f45547
harish kumar <harishpathania150@gmail.com>
2024-09-30 19:00:53 +0530

    learning about comparison operators

--- Lecture3-FundamentalsOfJs-2/Topic2-ComparisonOperators/Comparison-Operator.js
+++ Lecture3-FundamentalsOfJs-2/Topic2-ComparisonOperators/Comparison-Operator.js
@@ -23,4 +23,18 @@ console.log(5 <= 5);
 //  => 6 = (>=): Its known as (greater-than-equal-to) operator:It compares the values of two operands and returns true if the value of the left operand is greater than or equal to the value of the right operand:
 console.log(5 >= 5);
 
-// V.IMP = 
+// V.IMP = other comparison operators:which we only find in the javascript language(===,!==):
+// 1 = (===): strict-equality(comparison)-operator:It compares the values of two operands for equality and returns true if the values are equal and have the same type or same (data-type):
+// IMP = here we use simple (equality) operator:to check (5) with numerical value and ('5') of string:then we gonna get the output as (true):but its not ture:
+console.log(5 == "5");
+// but if we use the (strict) equality operator:then it gonna return us the (false) result:because (strict) operator not only check the (values) it also check there (data-types):
+
+console.log(5 === "5");
+
+// 2 = (!==): strict-inequality(comparison)-operator:It compares the values of two operands for inequality and returns true if the values are not equal and have the same type or same (data-type):
+// IMP = same thing happen in the (strict-inequality) operator:
+// -> check for (inequality) operator:
+console.log(5 != "5");
+
+// -> check for (strict-inequality) operator:
+console.log(5 !== 5);
