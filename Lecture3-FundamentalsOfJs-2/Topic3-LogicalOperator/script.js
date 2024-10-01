bded5face53aff17f0572678a6bad628e4fb0cb6
harish kumar <harishpathania150@gmail.com>
2024-10-01 19:05:14 +0530

    learning about Logical operators

---
+++ Lecture3-FundamentalsOfJs-2/Topic3-LogicalOperator/script.js
@@ -0,0 +1,4 @@
+// here we gonna be learn about (Logical-Operators):
+console.log('Learning about Logical Operators');
+// => 1 = Logical-Operator:(&&,||,!):these operators perform logical operations on boolean values and return boolean value as a result to us:
+//
