9f7f49a351cebbc8925edecdc920c0705bf40617
harish kumar <harishpathania150@gmail.com>
2024-10-02 13:18:39 +0530

    logical operators

--- Lecture3-FundamentalsOfJs-2/Topic3-LogicalOperator/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic3-LogicalOperator/script.js
@@ -3,3 +3,6 @@ console.log("Learning about Logical Operators");
 // => 1 = Logical-Operator:(&&,||,!):these operators perform logical operations on mulitple boolean values at once and return the another boolean value as a result to us:after checking and comparing all the multiple boolean values:
 // IMP = In more simpler way:when we have to (check) and (compare) more then one (boolean-values) and (conditions) or(statements) which gave us the result in the form of (boolean-value):then we use the (Logical-Operators) in the javascript:
 // V.IMP = but the more important is that we can also use the (non-boolean) values.which are the (truthy) and (false) values with in the (logical-operators):
+// => Truthy-values:non-empty strings, non-zero numbers, and objects:
+// => Falsy-values:false, 0, null, undefined, NaN, and "" (empty string):
+// V.IMP-(NOTE): with boolean-value's (conditions) and (statements) logical-operators work differenlty as if we compare it to the truthy,falsy (conditions) and (statements) values:
