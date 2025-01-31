1fb3e34d282afe3e8cde7d074110a7836e6122dc
harish kumar <harishpathania150@gmail.com>
2025-01-31 17:53:50 +0530

    revising about operators in js

--- Lecture3-FundamentalsOfJs-2/Topic2-ComparisonOperators/Comparison-Operator.js
+++ Lecture3-FundamentalsOfJs-2/Topic2-ComparisonOperators/Comparison-Operator.js
@@ -12,8 +12,8 @@ console.log("Learning About Comparison Operator");
 // => here gonna check them one by one:
 // => 1 = (==): Its known as (equality) operator:It compares the values of two operands for equality but without any type conversion. It returns true if the values are equal and have the same type or same (data-type):
 console.log(5 == 5);
-// => 2 = (!=):  Its known as (inequality) operator:It compares the values of two operands for inequality but without any type conversion. It returns true if the values are not equal and have the same type or same (data-type):
-console.log(5 != 6);
+// => 2 = (!=):  Its known as (inequality) operator:It compares the values of two operands for inequality but without any type conversion. It returns true if the values are not equal:
+console.log('inequality',5!= 6);
 // => 3 = (<): Its known as (less-than) operator:It compares the values of two operands and returns true if the value of the left operand is less than the value of the right operand:
 console.log(5 < 6);
 // => 4 = (>): Its known as (greater-than) operator:It compares the values of two operands and returns true if the value of the left operand is greater than the value of the right operand:
