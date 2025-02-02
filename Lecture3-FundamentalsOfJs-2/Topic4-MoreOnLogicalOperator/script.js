47ea46ebc3714c8302be914e652a1fa539c931d9
harish kumar <harishpathania150@gmail.com>
2025-02-02 19:54:56 +0530

    learning about operators in js

--- Lecture3-FundamentalsOfJs-2/Topic4-MoreOnLogicalOperator/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic4-MoreOnLogicalOperator/script.js
@@ -1,3 +1,5 @@
 console.log("Learning More About Logical Operator");
 // => (second) = here we gonna be learn about using logical operator with the non-boolean-values or we can say with the (truthy) and (falsy) values:
-// V.IMP-NOTE:truthy and falsy values can  also be called as primitive and non-primitive values:
+// V.IMP-NOTE:truthy and falsy values can  also be called as primitive and non-primitive values:and it only happens or works in the js:
+// -> Truthy-values:true,non-empty strings, non-zero numbers, and objects:
+// -> Falsy-values:false, 0, null, undefined, NaN, and "" (empty string):
