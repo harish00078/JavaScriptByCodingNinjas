cb3979b87bf4cfdfb5091740c228c1f21bb1ce93
harish kumar <harishpathania150@gmail.com>
2025-02-03 18:19:01 +0530

    learning about operators in js

--- Lecture3-FundamentalsOfJs-2/Topic4-MoreOnLogicalOperator/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic4-MoreOnLogicalOperator/script.js
@@ -12,3 +12,12 @@ console.log("Learning More About Logical Operator");
 // => V.IMP-NOTE:but the more important thing is that:which we have to learn here is that:if we use logical operator with the (truthy) and (falsy) values:then how the logic behind it is working that we are getting the result in the form of (truthy) and (falsy) values as well not in (boolean-values):
 // We have to Learn that how we are able to define that which value is gonna be our result:
 
+// Working-Explanation:Related to Logical-operators working with the (truthy) and (falsy) values:
+// -> (&&) - AND-Operator:
+// -> (||) - OR-Operator:
+// -> (!) - NOT-Operator:
+// => AND-Operator:
+// -> The && operator evaluates expressions from left to right.
+// -> It returns the first falsy value it encounters.and stop checking further-values:
+// -> If all values are truthy, it returns the last-truthy value.
+console.log("harish" && "kumar" && 0 && 1);
