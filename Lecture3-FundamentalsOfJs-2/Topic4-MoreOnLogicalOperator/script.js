83474df18e969dce962e377361a5776172d7a399
harish kumar <harishpathania150@gmail.com>
2025-02-03 18:11:02 +0530

    learning about operators in js

--- Lecture3-FundamentalsOfJs-2/Topic4-MoreOnLogicalOperator/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic4-MoreOnLogicalOperator/script.js
@@ -9,3 +9,6 @@ console.log("Learning More About Logical Operator");
 // Truthy-value -> Means (true):
 // Falsy-value -> Means (false):
 
+// => V.IMP-NOTE:but the more important thing is that:which we have to learn here is that:if we use logical operator with the (truthy) and (falsy) values:then how the logic behind it is working that we are getting the result in the form of (truthy) and (falsy) values as well not in (boolean-values):
+// We have to Learn that how we are able to define that which value is gonna be our result:
+
