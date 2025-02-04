ab3d4d78e53b881e41667be31b56d8d37b8aa314
harish kumar <harishpathania150@gmail.com>
2025-02-04 18:26:32 +0530

    learning about operators in js

---
+++ Lecture3-FundamentalsOfJs-2/Topic5-Precedence/script.js
@@ -0,0 +1,3 @@
+console.log("learning about Precedence in js");
+// -> The precedence of operators in JavaScript is determined by the order in which they are listed in the expression.
+// LIKE:In maths we have (BODMAS):
