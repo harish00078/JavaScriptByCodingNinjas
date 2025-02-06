cd756957988040b3acdcd53e1c50a0d3e54ca695
harish kumar <harishpathania150@gmail.com>
2025-02-06 18:04:20 +0530

    learning about operators in js

--- Lecture3-FundamentalsOfJs-2/Topic5-Precedence/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic5-Precedence/script.js
@@ -1,6 +1,5 @@
 console.log("learning about Precedence in js");
 // Precedence: It means (Priority):precedence refers to the order in which operators are evaluated in an expression. Operators with higher-precedence are executed before those with lower-precedence.
-// -> The precedence of operators in JavaScript is determined by the order in which they are listed in the expression.
 // LIKE:In maths we have (BODMAS):
 
 // => precedence refers to the order in which operators are evaluated in an expression. Operators with higher precedence are executed before those with lower precedence.
