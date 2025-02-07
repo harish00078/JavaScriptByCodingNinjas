eac29f30c9bae349c0e5a3365b4a8e9959d1b859
harish kumar <harishpathania150@gmail.com>
2025-02-07 20:28:52 +0530

    learning about operators in js

--- Lecture3-FundamentalsOfJs-2/Topic6-BitwiseOperator/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic6-BitwiseOperator/script.js
@@ -1,4 +1,8 @@
 // Learning about Bitwise-Operators:
 // Bitwise-Operators:Bitwise operators in JavaScript work at the binary-level:
-// -> they operate on 32-bit numbers in their binary form. These operators perform bitwise operations on (integer-values).
+// -> they operate on 32-bit numbers in their binary form. 
+// V.IMP: These operators perform bitwise operations on (integer-values).
 // IMP:(32-bit) means:Bitwise operators in JavaScript work on 32-bit signed integers. Even if you provide a decimal-number, JavaScript internally converts it into a 32-bit binary representation, performs the bitwise operation, and then converts it back to a decimal number.
+// -> Bitwise-Operators works with the (bits) or we can say with the (binary-number):where as with in Bitwise-operators (0) represents there self as (false) and (1) represents the (true):
+
+// here we have some bitwise operators:(&,|,^,~,<<,>>,>>> ):
