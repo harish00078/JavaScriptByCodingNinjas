6031114c3884a20ab3c242fe293b33f860ac8985
harish kumar <harishpathania150@gmail.com>
2025-04-12 16:44:26 +0530

    learning about operators in js

--- Lecture3-FundamentalsOfJs-2/Topic6-BitwiseOperator/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic6-BitwiseOperator/script.js
@@ -22,7 +22,7 @@
  * - >>> (Unsigned Right Shift)
  */
 
-// The bitwise AND (&) operator compares each bit of two numbers and returns a new bit-number which is going to be the result of the comparison:
+// [ Bitwise AND (&) operator]: compares each bit of two numbers and returns a new bit-number which is going to be the result of the comparison:
 // It compares them acc to the (AND) operator-properties:where each bit is 1 only if both corresponding bits in the input numbers are (1) otherwise, it's (0):
 
 let a = 5; // 00000000000000000000000000000101
@@ -31,5 +31,15 @@ let b = 3; // 00000000000000000000000000000011
 let c = a & b; // 00000000000000000000000000000001
 
 console.log(c); // 1
+
+
+// [Bitwise OR (|) operator]: compares each bit of two numbers and returns a new bit-number which is going to be the result of the comparison:
+// It compares them acc to the (OR) operator-properties:where each bit is (1) if at least one of the corresponding bits in the input numbers is (1) otherwise, it's (0):
+let d = 5; // 00000000000000000000000000000101
+let e = 3; // 00000000000000000000000000000011
+
+let f = a | b; // 00000000000000000000000000000111
+
+console.log(f); // 7
 
 
