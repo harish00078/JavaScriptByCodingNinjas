74c458208b523bb197af02d94e70e6d4f622b76c
harish kumar <harishpathania150@gmail.com>
2025-05-12 20:04:37 +0530

    learning about type-coercion

--- Lecture3-FundamentalsOfJs-2/Topic7-TypeCoercion/code-withNewLectures/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic7-TypeCoercion/code-withNewLectures/script.js
@@ -2,6 +2,5 @@ console.log('learning about type-coercion:  ');
 // => Type-Coercion:type-coercion is the process of changing the type of a value from one data-type to another:It basically occurs when javascript automatically converts a value of one data-type to another during an operation:
 
 // => Understand it through arithmetic-operations:
-// Type-Coercion act differently acc to operations:
-
-// IMP-NOTE:In arithmetic-operations:if we use (addition) operator its works or behaves differently rather than other arithmetic-operators (subtraction,multiplication,division,modulus,++,--) etc:
+// Type-Coercion acts differently acc to operations:
+// IMP-NOTE:In arithmetic-operations:if we use (addition) operator or operation then type-coercion it work or behaves differently rather than other arithmetic-operators or operations (subtraction,multiplication,division,modulus,++,--) etc:
