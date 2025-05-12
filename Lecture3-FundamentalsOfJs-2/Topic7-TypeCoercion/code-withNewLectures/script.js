8bc2b1e09198504dd268039818d1f56574b5305b
harish kumar <harishpathania150@gmail.com>
2025-05-12 18:28:57 +0530

    learning about type-coercion

--- Lecture3-FundamentalsOfJs-2/Topic7-TypeCoercion/code-withNewLectures/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic7-TypeCoercion/code-withNewLectures/script.js
@@ -1,1 +1,2 @@
 console.log('learning about type-coercion:  ');
+// Type-Coercion:type-coercion is the process of changing the type of a value from one data-type to another:It basically occurs when javascript automatically
