92b2081e82bbe46310d65fa6d53de4eda9209d3d
harish kumar <harishpathania150@gmail.com>
2025-05-16 19:28:48 +0530

    learning about type-coercion

--- Lecture3-FundamentalsOfJs-2/Topic7-TypeCoercion/code-withNewLectures/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic7-TypeCoercion/code-withNewLectures/script.js
@@ -12,3 +12,18 @@ console.log(5 + "5");
 // -> 2 = Other-Operators (subtraction,multiplication,division,modulus,increment,decrement):they work differently than addition operator in terms of type-coercion:
 // Here if we use one number and one string data-type value:then both of those values get converted into the form of number data-type instead of string data-type:and gonna get result in the form of numeric or arithmetic-operations:
 // V.IMP-NOTE: Also if the string cannot be converted to a number, then the  result will be NaN (Not a Number):
+
+// -> Subtraction:
+console.log(5 - "5");
+// -> Multiplication:
+console.log(5 * "5");
+// -> Division:
+console.log(5 / "5");
+// -> Modulus:
+console.log(5 % "5");
+// -> Increment:
+let x = '5';
+console.log(++x);
+// -> Decrement:
+let y = '5';
+console.log(--y);
