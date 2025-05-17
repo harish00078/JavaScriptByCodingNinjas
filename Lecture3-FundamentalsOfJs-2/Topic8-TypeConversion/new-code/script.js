6d48096c680212eebdf306c30f9fe6427cc07026
harish kumar <harishpathania150@gmail.com>
2025-05-17 21:48:21 +0530

    learning about type-conversion

--- Lecture3-FundamentalsOfJs-2/Topic8-TypeConversion/new-code/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic8-TypeConversion/new-code/script.js
@@ -17,3 +17,18 @@ console.log(typeof (num.toString()));
 
 // => 2: Converting to Number:
 // we have multiple-ways to do it:
+// -> 1 = first is (Number()) function:
+// -> 2 = second is (Unary (+) plus or addition-operator):
+// -> 3 = third is (parseInt()) function:
+// -> 4 = fourth is (parseFloat()) function:
+
+
+// IMP-NOTES:
+// -> 1: Convert (simple-string) or (non-number-string) to number:
+// IMP = instead of number-string if we only have the string:then what will be the answer of that string.if try to convert it into the number through (Number) method:
+// IMP = then in result we will have the (NaN):NaN is basically used to represents the (Not a number)  value in js:
+console.log(Number("value"));
+// -> 2: convert boolean-value to number:
+// IMP = here in result we will have the (1):because in bitwise system (1) represents the (true) value and (0) represents the (false) value:
+console.log(Number(true));
+// -> 3: every (integer) or (floating-point) value is considered as (number) or (number) data-type in javascript:
