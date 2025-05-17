1fa0ea7d96cb802fe7f2671ef45572a5f84ed394
harish kumar <harishpathania150@gmail.com>
2025-05-17 18:38:16 +0530

    learning about type-conversion

--- Lecture3-FundamentalsOfJs-2/Topic8-TypeConversion/new-code/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic8-TypeConversion/new-code/script.js
@@ -1,3 +1,5 @@
 console.log("Learn about: type-conversion");
 // Type-Conversion:
-// Type conversion is the process of changing the type of a value from one data-type to another. Several methods can be used in JavaScript to convert values to different data-types:
+// Type conversion is the process of converting a value from one data type to another.
+// In JavaScript, type-conversion is performed manually with the help of build-in methods.
+// Several methods are available to convert values between different data types.
