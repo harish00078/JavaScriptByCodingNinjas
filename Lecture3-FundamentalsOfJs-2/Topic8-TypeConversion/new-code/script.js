912d7c59f5e7aed9bd5b262da0797688fc950cfb
harish kumar <harishpathania150@gmail.com>
2025-05-19 18:24:07 +0530

    learning about type-conversion

--- Lecture3-FundamentalsOfJs-2/Topic8-TypeConversion/new-code/script.js
+++ Lecture3-FundamentalsOfJs-2/Topic8-TypeConversion/new-code/script.js
@@ -8,7 +8,7 @@ console.log("Learn about: type-conversion");
 // we have multiple-ways to do it:
 // -> 1 = first is (String()) function:
 // -> 2 = second is (toString()) method:
-// IMP-NOTE: both are used for type conversion to string in JavaScript, but String() can convert any data type to a string while toString() cannot convert null and undefined values to a string.
+
 let num1 = 123;
 // -> using (String) function:
 console.log(typeof (String(num1)));
@@ -33,7 +33,10 @@ console.log(typeof (+num2));
 
 
 // IMP-NOTES:
-// => Notes on Converting to String:
+// => Notes on Converting to string:
+// IMP: String() and toString() are used for type conversion to string in JavaScript, but String() can convert any data type to a string while toString() cannot convert null and undefined values to a string:
+
+// => Notes on Converting to number:
 // -> 1: Convert (simple-string) or (non-number-string) to number:
 // IMP = instead of number-string if we only have the string:then what will be the answer of that string.if try to convert it into the number through (Number) method:
 // IMP = then in result we will have the (NaN):NaN is basically used to represents the (Not a number)  value in js:
